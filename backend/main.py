f7db80fed59189b7e61ee53272cff2f19f238cb2
atbravo <at.bravo@uniandes.edu.co>
2023-04-25 16:35:14 -0500

    backConnecction

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,15 @@ from typing import Optional
 from DataModel import DataModel
 from fastapi import FastAPI
 import pandas as pd
-from plistlib import load
 from fastapi.middleware.cors import CORSMiddleware
+
+import numpy as np
+import pandas as pd
+import joblib
+from joblib import load
+import sys
+import json
+sys.modules['sklearn.externals.joblib'] = joblib
 
 app = FastAPI()
 
@@ -24,20 +31,9 @@ def read_item(item_id: int, q: Optional[str] = None):
    return {"item_id": item_id, "q": q}
 
 @app.post("/predict")
-def make_predictions(dataModel: DataModel):
-    df = pd.DataFrame(dataModel.dict(), columns=dataModel.dict().keys(), index=[0])
-    df.columns = dataModel.columns()
-    model = load("assets/modelo.joblib")
+def make_predictions():
+    datamodel = {'words': ['película basar arte frank frazetta mítico ilustrador fantasía alguno personaje pintura distribuidor muerte ser mejor ejemplo sorprendentemente animación lograr mantener sensación arte original bakshi ser bien conocer uso pesado rotocopio técnica rastrear secuencia acción vivo película ser excepción embargo dado tema película ser bastante realista personaje ser humano funcionar bastante bien realmente gustar aquí ser trama vez tener historia personaje interesante buen secuencia acción verdadero horrible villano hermoso nena película tener sensación mejor cómics conan ser sorprendente roy thomas ser escritor serie marvel cimmerio favorito ser grito lejano acción vivo mierda conan hablar película b género recomendar definitivamente']}
+    df = pd.DataFrame(datamodel)
+    model = load("C:/Users/andre/OneDrive/Documentos/Sistemas/BI/BI_Proyecto1/backend/assets/modelo.joblib")
     result = model.predict(df)
-    return result
-
-@app.post("/decisionTree")
-def make_predictions_decission_tree(data: DataModelList):
-   print(data)
-   df = convert_json_to_dataframe(data)
-   df.columns = DataModel.columns()
-   model = load("./models/Model_DecisionTree.joblib")
-   result = model.predict(df)
-   lists = result.tolist()
-   json_predict = json.dumps(lists)
-   return {"Predict_DT": json_predict}
+    return json.dumps({"result": result.tolist()})
